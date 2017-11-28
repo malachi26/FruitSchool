0444165778912670d36957dbbaafef4e39839ed1
Unknown <lyle.hart.3@gmail.com>
2017-11-28 18:54:08 -0500

    Added a Total Method for the Shopping Cart

--- FruitSchool/Logic/ShoppingCart.cs
+++ FruitSchool/Logic/ShoppingCart.cs
@@ -12,23 +12,23 @@ namespace FruitSchool.Logic
         public const string CartSessionKey = "CartID";
         private FruitSchoolContext _db = new FruitSchoolContext();
 
-        public void AddToCart (int id)
+        public void AddToCart(int id)
         {
             shoppingCartID = GetCartId();
 
             //var cartItem = _db.CartItems.SingleOrDefault(c => c.CartID == shoppingCartID && c.FruitID == id);
             //if (cartItem == null)
             //{
-                var cartItem = new CartItem
-                {
-                    CartItemID = Guid.NewGuid(),
-                    FruitID = id,
-                    CartID = shoppingCartID,
-                    //fruit = _db.Fruits.SingleOrDefault(f => f.FruitID == id),
-                    //Quantity = 1,
-                    DateCreated = DateTime.UtcNow
-                };
-                _db.CartItems.Add(cartItem);
+            var cartItem = new CartItem
+            {
+                CartItemID = Guid.NewGuid(),
+                FruitID = id,
+                CartID = shoppingCartID,
+                //fruit = _db.Fruits.SingleOrDefault(f => f.FruitID == id),
+                //Quantity = 1,
+                DateCreated = DateTime.UtcNow
+            };
+            _db.CartItems.Add(cartItem);
             //}
             //else
             //{
@@ -49,8 +49,8 @@ namespace FruitSchool.Logic
         public Guid GetCartId()
         {
             if (HttpContext.Current.Session[CartSessionKey] == null)
-            { 
-                if(!string.IsNullOrWhiteSpace(HttpContext.Current.User.Identity.Name))
+            {
+                if (!string.IsNullOrWhiteSpace(HttpContext.Current.User.Identity.Name))
                 {
                     HttpContext.Current.Session[CartSessionKey] = HttpContext.Current.User.Identity.Name;
                 }
@@ -66,12 +66,22 @@ namespace FruitSchool.Logic
                     _db.Cart.Add(newCart);
                 }
             }
-                       
+
             return Guid.Parse(HttpContext.Current.Session[CartSessionKey].ToString());
         }
         public List<CartItem> GetCartItems()
         {
             return _db.CartItems.Where(c => c.CartID == GetCartId()).ToList();
+        }
+
+        public decimal GetTotalInCart()
+        {
+            shoppingCartID = GetCartId();
+            decimal? total = decimal.Zero;
+            total = (from cartItems in _db.CartItems
+                     where cartItems.CartID == shoppingCartID
+                     select cartItems.Fruit.Price).Sum();
+            return total ?? decimal.Zero;
         }
     }
 }
